cef68d01ddcb35a2fb060f845e1873b538bcb392
Egor Sklyarov <egor.sklyarov.ru@gmail.com>
2023-12-20 21:02:04 +0100

    Refresh token on first load

--- sveltekit/src/routes/(auth)/+layout.js
+++ sveltekit/src/routes/(auth)/+layout.js
@@ -1,10 +1,14 @@
 import { redirect } from '@sveltejs/kit';
-import { get } from 'svelte/store';
-import { user } from '$lib/pocketbase';
+import { ClientResponseError } from 'pocketbase';
+import { pb } from '$lib/pocketbase';
 
 /** @type {import('./$types').LayoutLoad} */
 export const load = async ({ url }) => {
-	if (get(user) === null) {
+	try {
+		await pb.collection('users').authRefresh();
+	} catch (e) {
+		if (!(e instanceof ClientResponseError)) throw e;
+		pb.authStore.clear();
 		const relUrl = url.pathname + url.search;
 		throw redirect(302, `/login?next=${encodeURIComponent(relUrl)}`);
 	}
